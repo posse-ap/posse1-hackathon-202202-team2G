722ed2d9d2cd7358654b8b2956bd074fc7d7918f
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-02-15 10:58:41 +0900

    if文取り掛かり

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//選択肢ボタンの配列
 //ジャンルごとの曲名を配列に入れる
 //明るい
 const positive = [
@@ -54,3 +54,35 @@ shuffle(party);
 // console.log(intense);
 // console.log(cry);
 // console.log(party);
+
+
+//どの値がチェックされたかをカウントする
+//カウントされた値の合計値を基に条件分岐
+//if文に条件分岐された各々のジャンル先でシャッフル関数発火させる
+//もしもどこのジャンルにも属さない場合、５つのジャンルを１つの配列に入れ、その配列をシャッフルするように出力する（else文)
+function count1(){
+  var thisCount1 = 0;
+      thisCount1 = thisCount1 + 1;
+}
+function count2(){
+  var thisCount2 = 0;
+      thisCount2 = thisCount2 + 1;
+}
+function count3(){
+  var thisCount3 = 0;
+      thisCount3 = thisCount3 + 1;
+}
+function count4(){
+  var thisCount4 = 0;
+      thisCount4 = thisCount4 + 1;
+}
+function count5(){
+  var thisCount5 = 0;
+      thisCount5 = thisCount5 + 1;
+}
+
+console.log(thisCount1);
+console.log(thisCount2);
+console.log(thisCount3);
+console.log(thisCount4);
+console.log(thisCount5);
